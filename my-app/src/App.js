4b5ca85d05e608a9b8647a05fc3823b2cff1fe9b
Rhubert710 <73670513+Rhubert710@users.noreply.github.com>
2022-07-12 09:41:04 -0400

    Improved GET All Blogs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,20 +8,27 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
 
-const [serverJSON, setServerJSON] = useState({message: null})
-const urlEndpoint = "http://127.0.0.1:4000"
+const [serverJSON, setServerJSON] = useState([]);
+const urlEndpoint = "http://127.0.0.1:4000";
+
+const [sortField, setSortField] = useState("");
+const [sortOrder, setSortOrder] = useState("ASC");
+const [filterField, setFilterField] = useState("");
+const [filterValue, setFilterValue] = useState("");
+const [limit, setLimit] = useState(10);
+const [page, setPage] = useState(0);
+
 
 useEffect(() => {
   const fetchData = async () => {
-    const apiResponse = await fetch(`${urlEndpoint}/blogs/hello-blogs`);
+    const url = `${urlEndpoint}/blogs/all-blogs?sortField=${sortField}&sortOrder=${sortOrder}&filterField=${filterField}&filterValue=${filterValue}&limit=${limit}&page=${page}`
+    const apiResponse = await fetch(url);
     const apiJSON = await apiResponse.json();
-    console.log(apiJSON);
     setServerJSON(apiJSON);
     return;
   };
   fetchData();
-}, []);
-
+}, [sortField, sortOrder, filterField, filterValue, limit, page]);
 
   return (
     <div className="App">
@@ -30,7 +37,29 @@ useEffect(() => {
 
         <Routes>
 
-          <Route path="/blogs" element={ <BlogsPage message={serverJSON.message} /> } />
+          <Route path="/blogs" element={ <BlogsPage 
+          
+            message={serverJSON} 
+            
+            sortField={sortField}
+            setSortField={setSortField}
+
+            sortOrder={sortOrder}
+            setSortOrder={setSortOrder}
+
+            filterField={filterField}
+            setFilterField={setFilterField}
+
+            filterValue={filterValue}
+            setFilterValue={setFilterValue}
+
+            limit={limit}
+            setLimit={setLimit}
+
+            page={page}
+            setPage={setPage}
+            /> 
+            } />
         </Routes>
 
       </header>
--- my-app/src/pages/Blogs.js
+++ my-app/src/pages/Blogs.js
@@ -2,13 +2,97 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import React, { useState } from 'react';
 
 
-function BlogsPage (props)
-{
+const BlogsPage = (props) => {
     return (
         <div className="blogs-page">
-        <h1>Blogs Page</h1>
-        <p>Server Message: {props.message}</p>
-        </div>
+        <h1 style={{ display: "flex", justifyContent: "center" }}>Blogs Page</h1>
+        <p>
+          Select:{" "}
+          <select
+            defaultValue={props.sortField}
+            onChange={(e) => {
+              const value = e.target.value;
+              props.setSortField(value);
+            }}
+          >
+            <option value=""></option>
+            <option value="title">Title</option>
+            <option value="author">Author</option>
+            <option value="createdAt">Generated</option>
+          </select>{" "}
+          SortOrder:{" "}
+          <select
+            defaultValue={props.sortOrder}
+            onChange={(e) => {
+              const value = e.target.value;
+              props.setSortOrder(value);
+            }}
+          >
+            <option value="ASC">Ascend</option>
+            <option value="DESC">Descend</option>
+          </select>{" "}
+          Filter Field:{" "}
+          <select 
+          defaultValue={props.filterField}
+          onChange={(e)=> {
+            const value = e.target.value;
+            props.setFilterField(value)
+          }}
+          >
+            <option value=""></option>
+            <option value="title">Title</option>
+            <option value="author">Author</option>
+          </select>
+          <br></br>
+          Filter Value:{" "}
+          <input
+            type="text"
+            value={props.filterValue}
+            onChange={(e) => {
+              const value = e.target.value;
+              props.setFilterValue(value);
+            }}
+          ></input>{" "}
+          Limit:{" "}
+          <input
+            type="number"
+            value={props.limit}
+            onChange={(e) => {
+              const value = e.target.value;
+              props.setLimit(value);
+            }}
+          ></input>{" "}
+          Page:{" "}
+          <input
+            value={props.page}
+            onChange={(e) => {
+              const value = e.target.value;
+              props.setPage(value);
+            }}
+          ></input>
+          {props.message.map((blog, index) => {
+            return (
+              <div
+                key={`${blog}-${index}`}
+                style={{
+                  display: "flex",
+                }}
+              >
+                Id: {blog.id}
+                <br></br>
+                Title : {blog.title.toUpperCase()}
+                <br></br>
+                Author : {blog.author}
+                <br></br>
+                Text: {blog.text}
+                <br></br>
+                {blog.createdAt}
+              </div>
+            );
+          })}
+        </p>
+      </div>
+  
     )
-}
+  }
 export default BlogsPage;
